c9df70ef548c5bda4ec5c08a257a83e0ae69771d
joonda <kloudzero@kakao.com>
2024-12-11 20:37:41 +0900

    [Fix] post detail components

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,26 +1,17 @@
-import Link from "next/link"
-import { getPostDetail } from "@/lib/posts"
-import MDXContent from "@/components/postContent"
+import PostContent from "@/components/postContent";
+import { getPostDetail } from "@/lib/posts";
+
 
 const PostDetail = async ({
     params
 }: {
     params: { slug: string }
 }) => {
-    const { slug } = params;
-    const articleData = await getPostDetail(slug);
+    const { slug } = await params;
+    const PostDetail = await getPostDetail(slug);
     return (
-        <section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
-            <div className="flex justify-between font-poppins">
-                <Link href={"/"} className="flex flex-row gap-1 place-items-center">
-                    <p>back to home</p>
-                </Link>
-                <p>{articleData.date.toLocaleDateString()}</p>
-            </div>
-            <article className="prose">
-                <MDXContent source={articleData.contentHtml} />
-            </article>
-        </section>
+        <PostContent PostDetail={PostDetail} />
     )
 }
+
 export default PostDetail
--- src/components/postContent.tsx
+++ src/components/postContent.tsx
@@ -1,14 +1,25 @@
 "use client";
 
 import { MDXRemote } from "next-mdx-remote";
-import { MDXRemoteSerializeResult } from "next-mdx-remote";
+import Link from "next/link"
+import { AllPostContents } from "@/type/types";
 
-interface MdxContentProps {
-  source: MDXRemoteSerializeResult;
+export default function PostContent({
+  PostDetail
+}: {
+  PostDetail: AllPostContents
+}) {
+  return (
+    <section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
+      <div className="flex justify-between font-poppins">
+        <Link href={"/"} className="flex flex-row gap-1 place-items-center">
+          <p>back to home</p>
+        </Link>
+        <p>{PostDetail.date.toLocaleDateString()}</p>
+      </div>
+      <article className="prose">
+        <MDXRemote {...PostDetail.contentHtml} />
+      </article>
+    </section>
+  )
 }
-
-const MdxContent = ({ source }: MdxContentProps) => {
-  return <MDXRemote {...source} />;
-};
-
-export default MdxContent;
